16d0db19182dbe2cb6343af4aaf55fdc8038c12e
allan <allancheruiyot003@gmail.com>
2023-03-07 08:04:40 -0800

    ALX SE

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@ def uppercase(str):
     i = 1
     n = len(str)
     if n == 0:
-        return {0}
+        return {" "}
     for ch in str:
         if ord(ch) >= 97 and ord(ch) <= 122:
             print("{}".format(chr(ord(ch) - 32)), end="" if i != n else "\n")

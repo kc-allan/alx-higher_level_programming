ab88d5b70577db034ae349ea6c3c2ba4e2c22467
allan <allancheruiyot003@gmail.com>
2023-03-08 00:42:53 -0800

    ALX SE

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 def magic_calculation(a, b, c):
-    return (dis.dis(magic_calculation))
+    if a < b:
+        return c
+    elif c > b:
+        return a + b
+    else:
+        return a * b - c

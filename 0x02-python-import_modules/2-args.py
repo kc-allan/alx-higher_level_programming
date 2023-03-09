6f6b2179aaf6bce4bcbe4c21b993948a57621d1a
allan <allancheruiyot003@gmail.com>
2023-03-09 04:20:11 -0800

    ALX SE

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     if n == 1:
         print("0 arguments")
     elif n == 2:
-        print("1 argument:\n1 : {}".format(argv[1]))
+        print("1 argument:\n1: {}".format(argv[1]))
     else:
         print("{} arguments:".format(n - 1))
         for i in range(1, n, 1):

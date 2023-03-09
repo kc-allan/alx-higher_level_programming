3e592b56cf9ed2122d455d7164c0e2a43828139b
allan <allancheruiyot003@gmail.com>
2023-03-09 04:17:14 -0800

    ALX SE

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    n = len(argv)
+    if n == 1:
+        print("0 arguments")
+    elif n == 2:
+        print("1 argument:\n1 : {}".format(argv[1]))
+    else:
+        print("{} arguments:".format(n - 1))
+        for i in range(1, n, 1):
+            print("{}: {}".format(i, argv[i]))
+

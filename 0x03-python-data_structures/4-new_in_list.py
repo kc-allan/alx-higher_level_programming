baa6ba245085ddb8fee9c5140a690d395b62adc5
allan <allancheruiyot003@gmail.com>
2023-03-10 01:21:34 -0800

    ALX SE

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    new = my_list.copy()
+    if idx < 0 or idx > len(new) - 1:
+        return (new)
+    new[idx] = element
+    return (new)

e216b63c70ca8bf77b39867a1816c216126f505c
allan <allancheruiyot003@gmail.com>
2023-03-11 01:54:43 -0800

    ALX SE

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,8 +2,9 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return ("None")
-    for i in my_list:
-        n = my_list[0]
-        if i > n:
-            n = i
+    else:
+        for i in my_list:
+            n = my_list[0]
+            if i > n:
+                n = i
     return (n)

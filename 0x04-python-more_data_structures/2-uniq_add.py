af72e65a9648d29db30a10b572995169045cada1
allan <allancheruiyot003@gmail.com>
2023-03-15 07:20:06 -0700

    ALX SE

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    _sum = 0
+    my_list = sorted(my_list)
+    for i in range(len(my_list)):
+        if i == 0:
+            _sum += my_list[i]
+            continue
+        if (my_list[i] <= my_list[i - 1]) == False:
+            _sum += my_list[i]
+    return _sum

de0b002442e843bad105e30114257775a5de5651
allan <allancheruiyot003@gmail.com>
2023-03-16 11:52:57 -0700

    ALX SE

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    numerator = 0
+    denominator = 0
+    for i in my_list:
+        numerator += i[0] * i[1]
+        denominator += i[1]
+    return numerator / denominator

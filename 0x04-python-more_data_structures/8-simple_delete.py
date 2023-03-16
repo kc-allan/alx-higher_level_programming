d7c153e6b051d540d866bdd804bd9e4c6b1f6350
allan <allancheruiyot003@gmail.com>
2023-03-16 01:53:00 -0700

    ALX SE

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    del.a_dictionary[key]
+    try:
+        a_dictionary[key]
+    except KeyError:
+        return a_dictionary
+    del(a_dictionary[key])
     return a_dictionary

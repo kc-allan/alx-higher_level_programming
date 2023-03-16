31a97b15cd96628e3c70f4281d4060c520fcac4c
allan <allancheruiyot003@gmail.com>
2023-03-16 02:18:57 -0700

    ALX SE

--- 0x04-python-more_data_structures/10-main.py
+++ 0x04-python-more_data_structures/10-main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 best_score = __import__('10-best_score').best_score
 
-a_dictionary = {'John': 12}
+a_dictionary = {}
 best_key = best_score(a_dictionary)
 print("Best score: {}".format(best_key))
 

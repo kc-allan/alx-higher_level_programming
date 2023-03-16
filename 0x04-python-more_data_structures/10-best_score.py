39f152cc024ea089a2fc6620dd8804b5dd51546c
allan <allancheruiyot003@gmail.com>
2023-03-16 02:22:05 -0700

    ALX SE

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     best = ""
-    if a_dictionary == None:
+    if a_dictionary is None or a_dictionary == {}:
         return None
     for i in a_dictionary:
         maxn = a_dictionary[i]

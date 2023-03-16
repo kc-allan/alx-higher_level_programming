8d9457e163a5ffc78dfd59bbfb7f00cdc25a8f20
allan <allancheruiyot003@gmail.com>
2023-03-16 02:17:58 -0700

    ALX SE

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,6 +5,7 @@ def best_score(a_dictionary):
         return None
     for i in a_dictionary:
         maxn = a_dictionary[i]
+        best = i
         for k in a_dictionary:
             if a_dictionary[k] > maxn:
                 best = k

d955531f078e7a49fcb1ccc2e5866394248960a8
allan <allancheruiyot003@gmail.com>
2023-03-27 07:13:08 -0700

    ALX SE

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,8 +7,6 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="");
             k += 1
-        except IndexError:
-            break
         except TypeError:
             pass
         except ValueError:

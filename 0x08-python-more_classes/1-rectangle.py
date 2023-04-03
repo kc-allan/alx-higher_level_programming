d9dc03fb46516c8f51c1456624efb8a2b1d9f465
allan <allancheruiyot003@gmail.com>
2023-04-03 08:25:01 -0700

    ALX SE

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,6 +6,11 @@
 class Rectangle:
     """Represents the rectangle"""
     def __init__(self, width=0, height=0):
+        """Initialize Rectangle
+        args:
+            width: width of the rectangle
+            height: height of the rectangle
+        """
         self.width = width
         self.height = height
 

8eca907be3ded6f416e3058d5f8fb7d42f0e1599
allan <allancheruiyot003@gmail.com>
2023-04-03 08:34:46 -0700

    ALX SE

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -33,7 +33,7 @@ class Rectangle:
         """Height getter/setter"""
         return self.__height
 
-    @width.setter
+    @height.setter
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")

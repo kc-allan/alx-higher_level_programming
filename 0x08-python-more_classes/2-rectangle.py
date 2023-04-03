0555143428064462a6ffe4232e1defbfe95e2137
allan <allancheruiyot003@gmail.com>
2023-04-03 08:41:47 -0700

    ALX SE

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -47,4 +47,6 @@ class Rectangle:
 
     def perimeter(self):
         """Returns perimeter of the rectangle"""
+        if self.__height == 0 or self.__width == 0:
+            return 0
         return (2 * self.__height) + (2 * self.__width)

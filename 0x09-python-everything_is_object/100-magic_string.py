ae8c0f2ec5f50e5a363a4cc4240d8d34bff1af69
allan <allancheruiyot003@gmail.com>
2023-04-05 02:41:32 -0700

    ALX SE

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
-n = 0
 def magic_string():
-    global n
-    n += 1
-    res = ""
-    for i in range(n):
-        if i + 1 == n:
-            res += "Best School"
-        else:
-            res += "Best School, "
-    return res
+    magic_string.k = getattr(magic_string, 'k', 0) + 1
+    return ("Best School" * (magic_string.k - 1) + "Best School")

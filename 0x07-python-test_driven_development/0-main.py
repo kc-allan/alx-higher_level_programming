99fc575e037ba622722297d5418dc0ab6b5f6b5f
allan <allancheruiyot003@gmail.com>
2023-04-06 07:10:28 -0700

    ALX SE

--- 0x07-python-test_driven_development/0-main.py
+++ 0x07-python-test_driven_development/0-main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 add_integer = __import__('0-add_integer').add_integer
 
-print(add_integer(1, 2))
+print(add_integer(1.7976931348623157E+308, 1.7976931348623157E+308))
 print(add_integer(100, -2))
 print(add_integer(2))
 print(add_integer(100.3, -2))

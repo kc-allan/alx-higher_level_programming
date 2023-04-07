d5a79c1c4a5137e12e4997f6cb34178498907b52
allan <allancheruiyot003@gmail.com>
2023-04-07 16:04:54 -0700

    ALX SE

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -5,13 +5,9 @@
 def text_indentation(text):
     """newl8ne after characters"""
     ch = ".?:"
-    i = 0
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    while i < len(text):
-        print(text[i], end="")
-        if text[i] in ch:
-            print("\n")
-            i += 2
-        else:
-            i += 1
+    for k in ch:
+        if k in text:
+            text = text.replace(k, k + "\n\n")
+    print("\n".join(line.strip() for line in text.split("\n")))

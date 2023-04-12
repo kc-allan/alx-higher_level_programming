fa3169577cf99297ea0751cd40c3be09193909dd
allan <allancheruiyot003@gmail.com>
2023-04-12 09:52:01 -0700

    ALX SE

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/bin/usr/python3
+"""Checks for class instances"""
+
+
+def is_same_class(obj, a_class):
+    """Checks if obj is an instance of class"""
+    if type(obj) == a_class:
+        return True
+    return False

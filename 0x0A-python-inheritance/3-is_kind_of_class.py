cddd4041f38244290d4bae45c491e8e12089abba
allan <allancheruiyot003@gmail.com>
2023-04-12 10:17:28 -0700

    ALX SE

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""K8nd of classes"""
+
 def is_kind_of_class(obj, a_class):
+    """checks if obj is of class or inherited from a_class"""
     if isinstance(obj, a_class):
         return True
     return False

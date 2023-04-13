b6254185161bc13486c68312366f39ebe0703761
allan <allancheruiyot003@gmail.com>
2023-04-12 19:45:10 -0700

    ALX SE

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Reads aa file"""
+
+
+def read_file(filename=""):
+    """Prints out file xontents"""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")

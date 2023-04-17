e46c7e0a0317d46868d3602db15ee5351d364f5d
allan <allancheruiyot003@gmail.com>
2023-04-17 12:46:57 -0700

    ALX SE

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -6,5 +6,8 @@ class Base:
     """Defines the base class"""
     __nb_objects = 0
     def __init__(self, id=None):
-        self.id = id
-        Base.__nb_objects += 1
+        if not id is None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects

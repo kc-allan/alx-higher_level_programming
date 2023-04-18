69879dfe82de7e83c29d559b8de4f42c97ffef97
allan <allancheruiyot003@gmail.com>
2023-04-18 01:32:06 -0700

    ALX SE

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python3
-import unittest
+"""Tests for Base"""
 
-class BaseTests(unittest.TestCase):
-    def test_Base:
-        pass
+
+import os
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+
+
+class TestBase_init(unittest.TestCase):
+    """Defines unittests for init"""
+
+    def test_no_arg(self):
+        b1 = Base()
+        b2 = Base()
+        self.assertEqual(b1.id, b2.id - 1)
+
+
+unittest.main()

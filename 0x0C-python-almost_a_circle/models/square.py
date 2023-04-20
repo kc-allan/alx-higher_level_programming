75690f90cb9f7a60b38318bd1b81b50be915d4de
allan <allancheruiyot003@gmail.com>
2023-04-20 08:16:45 -0700

    ALX SE

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,7 +7,7 @@ class Square(Rectangle):
     """Represents a square"""
     def __init__(self, size, x=0, y=0, id=None):
         """Square initialization"""
-        super().__init(id, size, size, x, y)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         return f"[Square] (self.id) self.x/self.y - self.height"

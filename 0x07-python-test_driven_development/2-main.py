55060f3934891a3fdc1fee63bfef71ed1ee6f1d3
Allan <75929522+kc-allan@users.noreply.github.com>
2023-05-04 10:21:51 +0300

    Delete 0x07-python-test_driven_development directory

--- 0x07-python-test_driven_development/2-main.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-matrix_divided = __import__('2-matrix_divided').matrix_divided
-m1 = [[3, “9”], [15, 3]]
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6]
-]
-print(matrix_divided(m1, 2))
-print(matrix)

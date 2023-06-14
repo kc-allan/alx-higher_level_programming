2c6397e965d2a43f86f018c9bcd3f5ccb66d5d0f
Allan Kirui <kiruiallan401@gmail.com>
2023-06-14 11:33:00 -0700

    ALX SE

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""Reads file"""
+
 
 def read_file(filename=""):
+    """Defines o9ening and reading a file"""
+
     with open(filename) as f:
         cont = f.read()
         print(cont)

f56e6718110d3918921118dd90ef21ee4f6ed88e
Allan Kirui <kiruiallan401@gmail.com>
2023-06-14 12:06:35 -0700

    ALX SE

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -6,4 +6,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """Save my_obj to filename"""
     with open(filename, "w") as f:
-        json.dump(filename, f)
+        json.dump(my_obj, f)

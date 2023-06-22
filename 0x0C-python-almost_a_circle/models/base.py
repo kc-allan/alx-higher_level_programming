e89d6092a1b6f87764b9a233509040a923af9f40
Allan Kirui <kiruiallan401@gmail.com>
2023-06-22 08:35:21 -0700

    ALX SE

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -9,7 +9,7 @@ class Base:
 
     def __init__(self, id=None):
         """Initializes the class"""
-        if not id is None:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1

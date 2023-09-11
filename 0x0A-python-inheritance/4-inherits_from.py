519d31da902847819dc774aa81055a8e31b016f2
Allan Kirui <allancheruiyot003@gmail.com>
2023-09-11 18:10:14 +0000

    ALX SE

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Checks if obj inherits from a class"""
+
+
+def inherits_from(obj, a_class):
+    """Defines class checker"""
+
+    if issubclass(obj, a_class):
+        return True
+    return False

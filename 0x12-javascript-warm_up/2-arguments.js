28261b3a4bd0d9ec264774d7a7f105a1e14e4748
Allan Kirui <allancheruiyot003@gmail.com>
2023-12-29 11:40:15 +0300

    ALX SE

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,18 +1,10 @@
 #!/usr/bin/node
 
 const { argv } = require('node:process');
-const args = [];
 
-let count = 0;
-argv.forEach((val, index) => {
-  if (count >= 2) {
-    args[index - 2] = val;
-  }
-  count += 1;
-});
-if (args.length === 0) {
+if (argv.length === 0) {
   console.log('No argument');
-} else if (args.length === 1) {
+} else if (argv.length === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');

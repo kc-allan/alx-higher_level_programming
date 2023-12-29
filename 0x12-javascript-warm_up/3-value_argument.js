562667d47d3535e041da5a8069e106e534997475
Allan Kirui <allancheruiyot003@gmail.com>
2023-12-29 11:22:52 +0300

    ALX SE

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -5,15 +5,15 @@ const args = [];
 
 let count = 0;
 argv.forEach((val, index) => {
-	if (count >= 2) {
-		args[index - 2] = val;
-	}
-	count += 1;
+  if (count >= 2) {
+    args[index - 2] = val;
+  }
+  count += 1;
 });
 if (`${args}` === `${[]}`) {
-	console.log('No argument');
+  console.log('No argument');
 } else {
-	args.forEach(element => {
-		console.log(element);
-	});
+  args.forEach(element => {
+    console.log(element);
+  });
 }

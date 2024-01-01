ac1b7ee0a9e610186b84f9ad3689d4173f338c6a
Allan <allancheruiyot003@gmail.com>
2024-01-01 06:52:44 -0500

    ALX SE

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    this.width = w;
-    this.height = h;
+	if ((w > 0 && !(isNaN(w))) && (h > 0 && !(isNaN(h)))) {
+		this.width = w;
+		this.height = h;
+	}
   }
 }
 module.exports = Rectangle;

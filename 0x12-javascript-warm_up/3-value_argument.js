4dd32e21391e9de2e1285a5652ce6ac6cda61091
Allan <allancheruiyot003@gmail.com>
2024-01-01 05:54:34 -0500

    ALX SE

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,19 +1,8 @@
 #!/usr/bin/node
 
-const { argv } = require('node:process');
-const args = [];
-
-let count = 0;
-argv.forEach((val, index) => {
-  if (count >= 2) {
-    args[index - 2] = val;
-  }
-  count += 1;
-});
-if (`${args}` === `${[]}`) {
-  console.log('No argument');
+const arg = process.argv[2]
+if (arg !== undefined) {
+	console.log(arg);
 } else {
-  args.forEach(element => {
-    console.log(element);
-  });
+	console.log("No argument");
 }

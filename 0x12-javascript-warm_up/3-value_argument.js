7c37634139a5f0c00c7cb39cd1e22901c84a8aa9
Allan <allancheruiyot003@gmail.com>
2024-01-01 06:09:41 -0500

    ALX SE

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const arg = process.argv[2]
+const arg = process.argv[2];
 if (arg !== undefined) {
-	console.log(arg);
+  console.log(arg);
 } else {
-	console.log("No argument");
+  console.log('No argument');
 }

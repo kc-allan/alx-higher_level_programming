ea1923a31c04776cc2a636ac00d5c561adb9f0a9
Allan <allancheruiyot003@gmail.com>
2024-01-01 07:06:09 -0500

    ALX SE

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,19 +1,19 @@
 #!/usr/bin/node
 class Rectangle {
-	constructor (w, h) {
-	  if ((w > 0 && !(isNaN(w))) && (h > 0 && !(isNaN(h)))) {
-		  this.width = w;
-		  this.height = h;
-	  }
-	}
-
-	print () {
-		for (let i = 0; i < this.height; i++) {
-			for (let k = 0; k < this.width; k++) {
-				process.stdout.write("X");
-			}
-			console.log();
-		}
-	}
+  constructor (w, h) {
+    if ((w > 0 && !(isNaN(w))) && (h > 0 && !(isNaN(h)))) {
+      this.width = w;
+      this.height = h;
+    }
   }
-  module.exports = Rectangle;
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let k = 0; k < this.width; k++) {
+        process.stdout.write('X');
+      }
+      console.log();
+    }
+  }
+}
+module.exports = Rectangle;

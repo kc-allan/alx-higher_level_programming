ab2cc8b375fdd0b096d6b93e05ddcb138882bf53
Allan <allancheruiyot003@gmail.com>
2024-01-02 05:38:55 -0500

    ALX SE

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,12 +5,12 @@ Usage: ./1-filter_states.py <MySQL username> <MySQL password> <database_name>
 """
 from sys import argv
 import MySQLdb
-
-db = MySQLdb.connect(
-    host='localhost', port=3306, user=argv[1], passwd=argv[2], db=argv[3]
-)
-cur = db.cursor()
-cur.execute("SELECT * FROM states WHERE states.name LIKE 'N%'")
-rows = cur.fetchall()
-for row in rows:
-    print(row)
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host='localhost', port=3306, user=argv[1], passwd=argv[2], db=argv[3]
+    )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE states.name LIKE 'N%'")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)

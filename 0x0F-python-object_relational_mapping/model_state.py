a9beb5fe4613ec21cfec13c13ba5678c93b3c492
Allan <allancheruiyot003@gmail.com>
2024-01-02 11:39:14 -0500

    ALX SE

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -9,6 +9,7 @@ Base = declarative_base()
 
 
 class State(Base):
+    """Represesnts the class for table states in database"""
     __tablename__ = 'states'
     id = Column(Integer, nullable=False, unique=True, primary_key=True)
     name = Column(String(128), nullable=False)

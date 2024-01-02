b1c7a056acf43d5120bdc2e6f6b01c6e4c13e169
Allan <allancheruiyot003@gmail.com>
2024-01-02 05:54:19 -0500

    ALX SE

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
         host='localhost', port=3306, user=argv[1], passwd=argv[2], db=argv[3]
     )
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE states.name LIKE 'N%'")
+    cur.execute("SELECT * FROM states WHERE BINARY states.name LIKE 'N%'")
     rows = cur.fetchall()
     for row in rows:
         print(row)

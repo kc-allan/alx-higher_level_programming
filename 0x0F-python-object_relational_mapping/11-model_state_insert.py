7156f45c7577cab7dd11747f2f8414ae20768d0e
Allan <allancheruiyot003@gmail.com>
2024-01-02 13:21:26 -0500

    ALX SE

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Fetches all State object from the database that contain letter 'a'
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State, Base
+from sys import argv
+if __name__ == '__main__':
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            argv[1], argv[2], argv[3]
+        ), pool_pre_ping=True
+    )
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    session = SessionLocal()
+    session.add(State(name='Louisiana'))

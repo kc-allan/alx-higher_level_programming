88bbb9fc2b884f3b5fcfa8adaa2e546f5e98bfe1
Allan <allancheruiyot003@gmail.com>
2024-01-03 10:54:06 -0500

    ALX SE

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,13 +2,13 @@
 if (process.argv.length !== 3) {
   console.log('Provide a file path');
 } else {
-	const filePath = process.argv[2];
-	const fs = require('fs');
-	fs.readFile(filePath, 'utf-8', (err, data) => {
-		if (err) {
-			console.log(err);
-		} else {
-			console.log(data);
-		}
-	});
+  const filePath = process.argv[2];
+  const fs = require('fs');
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data);
+    }
+  });
 }

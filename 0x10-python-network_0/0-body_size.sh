8ccbe8833d56bffbf4752ae566fac9189f50cd5f
Allan <kiruiallan003@gmail.com>
2024-03-18 09:44:31 -0400

    ALX SE

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #A script that prints the content length of a http reponse
-echo $(curl -sI "https://$1") | $(echo "$res" | grep "Content-Length") | tr -d '\r' | wc -c
+res=$(curl -sI "https://$1")
+echo $(echo "$res" | grep "Content-Length") | tr -d '\r' | wc -c
